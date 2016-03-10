fd759556f39f8a82bcd8379ade45a566deeea0e2
Colt Steele <coltsteele1@gmail.com>
2016-03-09 21:21:19 -0800

    initial commit

---
+++ app.rb
@@ -0,0 +1,35 @@
+require 'sinatra'
+songs = [
+		{title: "My Way", year: 1969, url: "https://www.youtube.com/v/5AVOpNR2PIs"},
+		{title: "That's Life", year: 1969, url: "https://www.youtube.com/v/KIiUqfxFttM"},
+		{title: "Come Fly With Me", year: 1969, url: "https://www.youtube.com/v/Euci0_BBmNE"},
+		{title: "Luck Be A Lady", year: 1969, url: "https://www.youtube.com/v/QIfinjlLPF4"},
+		{title: "The Way You Look Tonight", year: 1969, url: "https://www.youtube.com/v/h9ZGKALMMuc"},
+		{title: "Fly Me To The Moon", year: 1969, url: "https://www.youtube.com/v/mQR0bXO_yI8"}
+	]
+
+get '/' do
+    erb :home
+end
+
+get '/about' do
+  "This is our first sinatra app!"
+end
+
+get '/songs' do
+	@songs = songs
+	erb :index
+end
+
+get '/songs/new' do
+	erb :new
+end
+
+get '/songs/:id' do
+	id = params[:id].to_i
+	@song = songs[id]
+	erb :show
+end
+
+post '/songs' do
+end
